e64b3b9813aad6cc687eceda4485d95bb993aa7f
skippy404 <skippycochrane@gmail.com>
2019-10-06 23:00:18 +0200

    removed depreciated install

--- install.sh
+++
@@ -1,9 +0,0 @@
-cd
-mkdir -p .vim/autoload
-mkdir -p .scripts/
-cp ~/repos/.dotfilesMinimal/miscfiles/Workspace.sh ~/.scripts
-cp ~/repos/.dotfilesMinimal/miscfiles/plug.vim ~/.vim/autoload
-
-ln -s ~/repos/.dotfilesMinimal/.bashrc ~/.bashrc
-ln -s ~/repos/.dotfilesMinimal/.vimrc ~/.vimrc
-source .bashrc
