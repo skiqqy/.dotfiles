848331b0172e96eb812bc766e910bfb287bb6427
Stephen Cochrane <skippycochrane@gmail.com>
2020-05-01 18:27:02 +0200

    last change

--- .testuninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Checks that configs where uninstalled.
-if [[ -f ~/.backup/.vim || -f ~/.backup/.vimrc || -f ~/.backup/.bashrc || -f ~/.backup/.zshrc ]]; then
-	exit 1
-fi
-
-echo "Uninstall test completed"
